8b5d9a1026b6f5d6cd51c819175f6c379a865e26
원치현 <heungeob1003@gmail.com>
2021-10-17 17:38:52 +0900

    Add funcComp,classComp Components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,31 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="Container">
+      <h1>Hello World</h1>
+      <FuncComp></FuncComp> 
+      <ClassComp></ClassComp>      
+    </div>
+  );
+}
+
+function FuncComp() {
+  return(
+    <div className="container">
+        <h2>function style component</h2>
+    </div>
+  );
+}
+
+class ClassComp extends React.Component{
+  render() {
+    return (
+        <div className="container">
+            <h2>class style component</h2>
+        </div>
+    );
+  }
 }
 
 export default App;
