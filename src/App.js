ea94d77fe1b4eeaf809ba247cd8581b21cb56d2d
원치현 <heungeob1003@gmail.com>
2021-10-18 15:52:03 +0900

    Build date button that print now date by using react hook (func style component)

--- src/App.js
+++ src/App.js
@@ -15,15 +15,26 @@ function FuncComp(props) {
   var numberState = useState(props.initNumber);
   var number = numberState[0];
   var setNumber = numberState[1];
+
+  var dateState = useState((new Date()).toString());
+  var _date = dateState[0];
+  var setDate = dateState[1];
+
   console.log('numberState', numberState);
 
   return(
     <div className="container">
         <h2>function style component</h2>
         <p>Number: {number}</p>
+        <p>Date: {_date}</p>
         <input type="button" value="random" onClick={
           function() {
             setNumber(Math.random());
+          }
+        }></input>
+        <input type="button" value="date" onClick={
+          function(e){
+            setDate(new Date().toString());
           }
         }></input>
     </div>
